1265866f4ddfdaab7ba72cf1839c4046cbe87522
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-11 16:30:50 -0500

    wip

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""0. Gather data from an API"""
+import json
+import sys
+import urllib.request
+
+base_url = "https://jsonplaceholder.typicode.com/"
+
+# Write a Python script that, using this REST API,
+if __name__ == "__main__":
+
+    if len(sys.argv) <= 1:
+        print("Please enter an employee ID.")
+    
+    else:
+        employee_id = int(sys.argv[1])
